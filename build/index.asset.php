5ebc3f88be4f3abe0fc61dca7c569c38cc44bb27
jacobschweitzer <3016425+jacobschweitzer@users.noreply.github.com>
2025-07-13 12:43:23 -0500

    Logo fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => 'b13decbb1ce1ad2360b7');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => 'd26efd509f34fedba832');
