fb36e61377d8aa7820ae2d5f4538786c91be5844
jacobschweitzer <3016425+jacobschweitzer@users.noreply.github.com>
2025-11-22 13:43:01 -0600

    Save default quality setting if not set

--- inc/class-image-provider.php
+++ inc/class-image-provider.php
@@ -179,15 +179,23 @@ abstract class KaiGen_Image_Provider implements KaiGen_Image_Provider_Interface 
     public static function get_quality_setting() {
         $quality_settings = get_option('kaigen_quality_settings', []);
 
+        // Ensure we have an array to work with.
+        if ( ! is_array( $quality_settings ) ) {
+            $quality_settings = [];
+        }
+
         // If the quality setting is not set, check for the old option and migrate it.
-        if (!isset($quality_settings['quality'])) {
+        if ( ! isset( $quality_settings['quality'] ) ) {
             $legacy_quality_setting = get_option('kaigen_quality_setting');
-            if ($legacy_quality_setting) {
+            
+            if ( ! empty( $legacy_quality_setting ) && in_array( $legacy_quality_setting, ['low', 'medium', 'high'], true ) ) {
                 $quality_settings['quality'] = $legacy_quality_setting;
                 update_option('kaigen_quality_settings', $quality_settings);
                 delete_option('kaigen_quality_setting');
             } else {
-                return 'medium'; // Default value if neither exists
+                // Default to medium quality if neither exists.
+                $quality_settings['quality'] = 'medium';  
+                update_option('kaigen_quality_settings', $quality_settings);
             }
         }
 
