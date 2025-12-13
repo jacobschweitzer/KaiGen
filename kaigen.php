e816dae133d1d5440ee5b2d09ffb9de9b2e2d682
jacobschweitzer <3016425+jacobschweitzer@users.noreply.github.com>
2025-12-13 11:30:58 -0600

    0.2.5 version update

--- kaigen.php
+++ kaigen.php
@@ -4,7 +4,7 @@
  * Description:       A plugin to generate images using AI.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.2.4
+ * Version:           0.2.5
  * Author:            Jacob Schweitzer
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
