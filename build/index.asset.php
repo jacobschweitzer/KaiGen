cdbc5407a809bf92c466eee0dc16fe0f8a8617c6
jacobschweitzer <3016425+jacobschweitzer@users.noreply.github.com>
2026-01-18 16:07:42 -0600

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => '75d7006d3a754a269677');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => 'e88423045db695e963dc');
