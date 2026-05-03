dcd8838d670d4a738dcbd063b0f0697f71984ebe
Jacob Schweitzer <allambition@gmail.com>
2026-05-03 18:54:03 -0500

    Add mediaUtils.js for helper functions

--- src/filters/addMediaReplaceFlowFilter.js
+++ src/filters/addMediaReplaceFlowFilter.js
@@ -4,44 +4,11 @@ import { useState } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import AITab from '../components/AITab';
 import GenerateImageModal from '../components/GenerateImageModal';
-
-const isImageOnlyMedia = ( allowedTypes = [] ) => {
-	return (
-		allowedTypes.length > 0 &&
-		allowedTypes.every(
-			( allowedType ) =>
-				allowedType === 'image' || allowedType.startsWith( 'image/' )
-		)
-	);
-};
-
-const getSelectedImageBlock = () =>
-	wp.data.select( 'core/block-editor' ).getSelectedBlock();
-
-const getCurrentImage = ( selectedBlock ) => {
-	if ( ! selectedBlock?.attributes?.url ) {
-		return null;
-	}
-
-	return {
-		url: selectedBlock.attributes.url,
-		id: selectedBlock.attributes.id,
-		alt: selectedBlock.attributes.alt || '',
-	};
-};
-
-const shouldDisplayForSelectedImageBlock = ( props, selectedBlock ) => {
-	const hasApiKey = wp.data
-		.select( 'core/editor' )
-		?.getEditorSettings()?.kaigen_has_api_key;
-
-	return (
-		isImageOnlyMedia( props.allowedTypes ) &&
-		! props.multiple &&
-		selectedBlock?.name === 'core/image' &&
-		hasApiKey
-	);
-};
+import {
+	getCurrentImage,
+	getSelectedImageBlock,
+	shouldDisplayForSelectedImageBlock,
+} from './mediaUtils';
 
 addFilter(
 	'editor.MediaReplaceFlow',
