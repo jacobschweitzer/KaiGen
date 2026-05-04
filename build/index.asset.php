cb206aaf9205121ed815c4094f111ba75e080242
Jacob Schweitzer <allambition@gmail.com>
2026-05-03 19:11:27 -0500

    Rebuild editor assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => '9b65fefc4347d73fe42d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => 'c09c4619069bf76a3f62');
