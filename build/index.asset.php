74b1f32cb101c8a1f1c1dbcb778287e8c8d1ad2c
Jacob Schweitzer <allambition@gmail.com>
2026-05-23 12:20:32 -0500

    Includes newly built files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => 'c09c4619069bf76a3f62');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => '89e03a272c6e98ffe4e4');
