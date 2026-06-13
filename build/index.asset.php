b129ca0f979b6df52b959b51d5ced90ae5cb7725
Jacob Schweitzer <allambition@gmail.com>
2026-06-13 16:20:12 -0500

    Build composer control assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'afbf30c5d1a4e219708a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '5500ddcd3a85c3346f83');
