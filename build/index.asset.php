25031daad7209f6adb870ec55845e1a214eab8f9
Jacob Schweitzer <allambition@gmail.com>
2026-07-17 17:03:24 -0300

    Address PR review feedback (#58)
    - Rebuild committed editor assets for structured REST errors

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '5eed9b6dddb26b809139');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '935a3b196deab532719d');
